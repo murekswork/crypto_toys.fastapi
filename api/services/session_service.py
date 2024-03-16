e37a348f5bdf59f6f5ae2fea30cfdcfae9b4763d
murekswork <84355451+murekswork@users.noreply.github.com>
2024-03-16 03:30:35 +0300

    Update session_service.py

--- api/services/session_service.py
+++ api/services/session_service.py
@@ -7,7 +7,7 @@ from api.schemas.follow_lists import FollowListSchema
 
 class SessionService:
 
-    def __init__(self, request: Request):
+    def __init__(self, request: Request) -> None:
         self.request = request
         self.session = self.request.session
 
@@ -27,23 +27,34 @@ class SessionService:
         return [FollowListSchema(name=key, coins=self.follow_lists[key]) for
                 key in self.follow_lists]
 
-    async def get_follow_list(self, list_name) -> FollowListSchema:
+    async def get_follow_list(
+        self, list_name
+    ) -> FollowListSchema:
+        
         if self.follow_lists != '{}':
             request_follow_list = self.follow_lists.get(list_name)
             print(request_follow_list, 'Its your list')
             if request_follow_list is None:
 
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                    detail='You dont have list with this name!')
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail='You dont have list with this name!'
+                )
 
-            return FollowListSchema(name=list_name,
-                                    coins=request_follow_list)
+            return FollowListSchema(
+                name=list_name,
+                coins=request_follow_list
+            )
 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='You dont have any follow lists!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='You dont have any follow lists!'
+        )
 
-    async def delete_follow_list(self, list_name: str) -> list[
-            FollowListSchema]:
+    async def delete_follow_list(
+        self, list_name: str
+    ) -> list[FollowListSchema]:
+        
         if self.follow_lists != '{}':
             if self.follow_lists.get(list_name) is not None:
                 self.follow_lists.pop(list_name)
@@ -57,7 +68,9 @@ class SessionService:
 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-    async def create_follow_list(self, new_list_name: str) -> FollowListSchema:
+    async def create_follow_list(
+        self, new_list_name: str
+    ) -> FollowListSchema:
 
         lst = self.follow_lists.get(new_list_name)
         if lst is None:
@@ -68,28 +81,43 @@ class SessionService:
         raise HTTPException(status_code=403,
                             detail='You already have list with same name!')
 
-    async def add_to_follow_list(self, list_name: str, coin_id: int) -> FollowListSchema:
+    async def add_to_follow_list(
+        self, 
+        list_name: str, 
+        coin_id: int
+    ) -> FollowListSchema:
 
         lst = self.follow_lists.get(list_name)
 
         if lst is not None:
 
-            lst_schema = FollowListSchema(list_name=list_name, coins=lst)
+            lst_schema = FollowListSchema(
+                list_name=list_name, 
+                coins=lst
+            )
+            
             if coin_id in lst_schema.coins:
 
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
-                    detail=f'You already have coin with id {coin_id} in list {list_name}')
+                    detail=f'You already have coin with id {coin_id} in list {list_name}'
+                )
 
             lst_schema.coins.append(coin_id)
             self.follow_lists[list_name] = lst_schema.coins
             self.session['follow_lists'] = self.follow_lists
             return lst_schema
 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='You dont have list with this name!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='You dont have list with this name!'
+        )
 
-    async def remove_from_follow_list(self, list_name: str, coin_id: int):
+    async def remove_from_follow_list(
+        self, 
+        list_name: str, 
+        coin_id: int
+    ) -> FollowListSchema:
 
         lst = self.follow_lists.get(list_name)
 
@@ -101,8 +129,12 @@ class SessionService:
                 self.session['follow_lists'] = self.follow_lists
                 return FollowListSchema(name=list_name, coins=lst)
 
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                                detail=f'You dont have {coin_id} in list {list_name}')
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f'You dont have {coin_id} in list {list_name}'
+            )
 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='You dont have list with this name')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='You dont have list with this name'
+        )
