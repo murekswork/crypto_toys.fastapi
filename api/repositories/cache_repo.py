387f2b34057712cae228b0b291ccc0815cc55564
murekswork <84355451+murekswork@users.noreply.github.com>
2024-03-26 01:39:48 +0300

    Cleanup

--- api/repositories/cache_repo.py
+++ api/repositories/cache_repo.py
@@ -12,24 +12,24 @@ class CacheRepository:
     async def set(self, key: str, value: str):
         try:
             await self.cache.set(key, value)
-            logging.warning(f'Setting key {key} to {value}')
         except Exception as e:
-            raise HTTPException(status_code=500,
-                                detail=f'Cache database connection error! {e}')
+            raise HTTPException(
+                status_code=500,
+                detail=f'Cache database connection error! {e}'
+            )
 
     async def get(self, key: str) -> str | None:
         try:
-            logging.warning(f'Looking for {key} in cache...')
             cached = await self.cache.get(key)
             if cached is not None:
                 logging.warning('Found cached value!')
                 return cached
-            logging.warning('Cache does not exists!')
-            logging.warning(cached)
             return None
         except Exception as e:
-            raise HTTPException(status_code=500,
-                                detail=f'Cache database connection error! {e}')
+            raise HTTPException(
+                status_code=500,
+                detail=f'Cache database connection error! {e}'
+            )
 
     async def set_coin_chart_cache(self,
                                    coin_id: int,
@@ -40,5 +40,7 @@ class CacheRepository:
             await self.cache.set(f'{coin_id}/chart_cache/{timestamp}',
                                  value, ex=60)
         except Exception as e:
-            raise HTTPException(status_code=500,
-                                detail=f'Could not set chart cache for {coin_id}! {e}')
+            raise HTTPException(
+                status_code=500,
+                detail=f'Could not set chart cache for {coin_id}! {e}'
+            )
