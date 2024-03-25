48991c0d844dd71d5496c2a47939dd72e1c91907
murekswork <84355451+murekswork@users.noreply.github.com>
2024-03-26 01:45:56 +0300

    Update bubbles.js

--- api/static/js/items/bubbles.js
+++ api/static/js/items/bubbles.js
@@ -1,12 +1,9 @@
-// Get the canvas element and its context
 var canvas = document.getElementById('bubbleCanvas');
 var ctx = canvas.getContext('2d');
 
-// Set the canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Define the bubble class
 class Bubble {
 
     constructor(x, y, radius, dx, dy, name, text, symbol) {
@@ -42,16 +39,11 @@ class Bubble {
         ctx.fill();
         ctx.closePath();
 
-
-        // Draw the text separately to prevent it from being affected by the shadow
         ctx.beginPath();
         ctx.font = '15px red';
         ctx.fillText(this.symbol, this.x, this.y + (this.radius * 1.2))
         ctx.fillStyle = 'rgba(0,0,0)';  // Adjust text color as needed
         ctx.closePath();
-
-
-      // ... (other methods)
     }
 
     handleCollision(otherBubble) {
@@ -61,12 +53,8 @@ class Bubble {
       const sumRadii = this.radius + otherBubble.radius;
 
       if (distance < (sumRadii/1.1)) {
-        // Bubbles are colliding, handle collision logic here
-
-        // Calculate the overlap distance
         const overlap = sumRadii - distance;
 
-        // Move the bubbles away from each other based on overlap
         const angle = Math.atan2(dy, dx);
         const moveX = (overlap / 2) * Math.cos(angle);
         const moveY = (overlap / 2) * Math.sin(angle);
@@ -76,7 +64,6 @@ class Bubble {
         otherBubble.x += moveX * 0.1;
         otherBubble.y += moveY * 0.1;
 
-        // Example: Swap velocities for a simple bounce effect
         const tempDx = this.dx;
         const tempDy = this.dy;
         this.dx = otherBubble.dx;
@@ -87,12 +74,9 @@ class Bubble {
     }
 
     update() {
-    // Check if the bubble is about to go out of the canvas
     if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-      // Reverse the x-velocity to bounce off the vertical edges
       this.dx = -this.dx;
 
-      // Adjust the position to keep the bubble inside the canvas
       if (this.x + this.radius > canvas.width) {
         this.x = canvas.width - this.radius;
       } else {
@@ -101,10 +85,8 @@ class Bubble {
     }
 
     if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-      // Reverse the y-velocity to bounce off the horizontal edges
       this.dy = -this.dy;
 
-      // Adjust the position to keep the bubble inside the canvas
       if (this.y + this.radius > canvas.height) {
         this.y = canvas.height - this.radius;
       } else {
@@ -112,29 +94,20 @@ class Bubble {
       }
     }
 
-    // Check for collisions with other bubbles
     for (let i = 0; i < bubbles.length; i++) {
       if (this !== bubbles[i]) {
         this.handleCollision(bubbles[i]);
       }
     }
 
-    // Update the position
     this.x += this.dx;
     this.y += this.dy;
 
-    // Draw the bubble
     this.draw();
   }
 
 }
 
-// Create an array to store the bubbles
-
-
-// Create bubbles and add them to the array
-
-// Animation loop
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -152,13 +125,11 @@ canvas.addEventListener('mousedown', function(event) {
   const mouseX = event.clientX - canvas.getBoundingClientRect().left;
   const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-  // Check if the mouse is over any bubble
   for (let i = 0; i < bubbles.length; i++) {
     const bubble = bubbles[i];
     const distance = Math.sqrt(Math.pow(mouseX - bubble.x, 2) + Math.pow(mouseY - bubble.y, 2));
 
     if (distance < bubble.radius) {
-      // Set the selected bubble
       selectedBubble = bubble;
       break;
     }
@@ -166,7 +137,6 @@ canvas.addEventListener('mousedown', function(event) {
 });
 
 canvas.addEventListener('mouseup', function(event) {
-  // Reset the selected bubble
   selectedBubble = null;
 });
 
@@ -175,8 +145,7 @@ canvas.addEventListener('mousemove', function(event) {
     const mouseX = event.clientX - canvas.getBoundingClientRect().left;
     const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-    // Move the selected bubble toward the mouse
-    const speed = 1; // You can adjust the speed
+    const speed = 1;
     const angle = Math.atan2(mouseY - selectedBubble.y, mouseX - selectedBubble.x);
     selectedBubble.dx = Math.cos(angle) * speed;
     selectedBubble.dy = Math.sin(angle) * speed;
@@ -192,12 +161,11 @@ canvas.addEventListener('click', function(event) {
     const distance = Math.sqrt(Math.pow(mouseX - bubble.x, 2) + Math.pow(mouseY - bubble.y, 2));
 
     if (distance < bubble.radius) {
-      // Bubble is clicked, log its information to the console
       console.log(`Clicked Bubble: Name - ${bubble.name}, Radius - ${bubble.radius}, Position - (${bubble.x}, ${bubble.y})`);
       action_coin_name = bubble.name
       coin_info_modal_manager.openCoinInfoModal(bubble.name)
 
-      break;  // If you want to log only the top bubble (in case of overlapping bubbles), remove this line
+      break;
     }
   }
 });
@@ -205,11 +173,9 @@ canvas.addEventListener('click', function(event) {
 
 function drawBubbles(data, field) {
     bubbles = [];
-
-    // Sort the data based on the field value
+    
     data.sort((a, b) => parseFloat(a[field]) - parseFloat(b[field]));
 
-    // Calculate quantiles for determining bubble sizes
     let quantiles = [];
     for (let i = 0; i < 7; i++) {
         quantiles.push(parseFloat(data[Math.floor(i * data.length / 7)][field]));
